7583c4d2068d2f56249adfd18d5448cad9b4d426
Roberto Estrada <restrada@paridin.com>
2019-11-10 01:27:49 -0600

    update config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,13 @@
 use Mix.Config
 
-config :mas_mensajes,
-  token: System.get_env(MAS_MENSAJES_TOKEN) ||
+token = System.get_env("MAS_MENSAJES_TOKEN") ||
   raise """
   environment variable MAS_MENSAJES_TOKEN is missing.
   For example: MAS_MENSAJES_TOKEN=eyJ0eXAi...OiJKV1QiLCJhbGci
   """
+
+config :mas_mensajes,
+  token: token
 # And access this configuration in your application as:
 #
 #     Application.get_env(:mas_mensajes, :token)
